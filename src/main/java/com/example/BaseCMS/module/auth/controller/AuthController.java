e83e29ed1b7270902046e48fe69a5d8389bd0055
Hung Hoang <hoangmanhhung008009@gmail.com>
2025-04-08 21:53:20 +0700

    Revert "update"
    This reverts commit 4e70a9c01d6a65145f274b05db36ba0b452279d2.

--- src/main/java/com/example/BaseCMS/module/auth/controller/AuthController.java
+++ src/main/java/com/example/BaseCMS/module/auth/controller/AuthController.java
@@ -1,23 +1,22 @@
 package com.example.BaseCMS.module.auth.controller;
 
-import com.example.BaseCMS.module.auth.request.LoginRequest;
 import com.example.BaseCMS.module.auth.service.AuthService;
-import lombok.*;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 @RequestMapping("/api/v1/auth")
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody() LoginRequest loginRequest) {
-        authService.login(loginRequest.getUsername(), loginRequest.getPassword());
+    public ResponseEntity<Void> login(@RequestBody String username, @RequestBody String password) {
+        authService.login(username, password);
         return ResponseEntity.ok().build();
     }
     @PostMapping("/register")
--- src/main/java/com/example/BaseCMS/module/auth/service/AuthService.java
+++ src/main/java/com/example/BaseCMS/module/auth/service/AuthService.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-    public void login(String username, String password) {
-
+    public String login(String username, String password) {
+        // Implement your login logic here
+        return "Login successful for user: " + username;
     }
 
     public String register(String username, String password) {
-
+        // Implement your registration logic here
         return "Registration successful for user: " + username;
     }
 }
