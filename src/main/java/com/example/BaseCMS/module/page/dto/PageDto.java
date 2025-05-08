53157691676fcc395691a9f2254ee8b394a3f4f9
Hung Hoang <hoangmanhhung008009@gmail.com>
2025-05-08 15:58:26 +0700

    update

--- src/main/java/com/example/BaseCMS/module/page/dto/PageDto.java
+++ src/main/java/com/example/BaseCMS/module/page/dto/PageDto.java
@@ -3,9 +3,11 @@ package com.example.BaseCMS.module.page.dto;
 import com.example.BaseCMS.module.page.PageEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PageDto {
     private Long id;
     private String title;
