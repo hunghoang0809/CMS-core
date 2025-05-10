0a9a27104a896c72187ffb087a8c4b4d763464df
Hung Hoang <hoangmanhhung008009@gmail.com>
2025-05-11 00:49:22 +0700

    update

--- src/main/java/com/example/BaseCMS/config/CorsConfig.java
+++ src/main/java/com/example/BaseCMS/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsWebFilter() {
          CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("*", "http://localhost:3000", "http://160.191.244.248:3010"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.addAllowedHeader("*");
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "X-Authorization", "Proxy-Authorization", "Proxy-Connection", "X-Requested-With"));
