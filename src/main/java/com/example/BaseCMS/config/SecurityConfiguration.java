150f2cfc45c01f410795d2618f45d5b21e3cb14e
Hung Hoang <hoangmanhhung008009@gmail.com>
2025-05-11 00:46:34 +0700

    update

--- src/main/java/com/example/BaseCMS/config/SecurityConfiguration.java
+++ src/main/java/com/example/BaseCMS/config/SecurityConfiguration.java
@@ -73,18 +73,5 @@ public class SecurityConfiguration {
     }
 
 
-        @Bean
-        public CorsWebFilter corsWebFilter() {
-            final CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(Collections.singletonList("*"));
-            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-            configuration.addAllowedHeader("*");
-            configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "X-Authorization", "Proxy-Authorization", "Proxy-Connection", "X-Requested-With"));
-            configuration.setExposedHeaders(Arrays.asList("Location", "Content-Disposition"));
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", configuration);
-            return new CorsWebFilter(source);
-        }
-
 
 }
