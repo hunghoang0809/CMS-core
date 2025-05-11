5700defbbc25859039028d27ad2de48cd1ed3b43
Hung Hoang <hoangmanhhung008009@gmail.com>
2025-05-11 23:09:04 +0700

    update

--- src/main/java/com/example/BaseCMS/config/CorsConfig.java
+++ src/main/java/com/example/BaseCMS/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of( "*"));
+        config.addAllowedOriginPattern("*");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Location", "Content-Disposition"));
