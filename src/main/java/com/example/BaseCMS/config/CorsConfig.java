1aa41978227cab2c6dd137e56f7ab703160446a1
Hung Hoang <hoangmanhhung008009@gmail.com>
2025-05-20 16:08:58 +0700

    update

--- src/main/java/com/example/BaseCMS/config/CorsConfig.java
+++ src/main/java/com/example/BaseCMS/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOriginPattern("*");
         config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedOriginPatterns(List.of("https://api.antechgroup.com.vn"));
+        config.setAllowedOriginPatterns(List.of("https://api.antechgroup.com.vn", "localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Location", "Content-Disposition"));
