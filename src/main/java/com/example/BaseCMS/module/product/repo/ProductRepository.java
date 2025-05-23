5a4a6931785571afc10223dc07334780c2aec75a
Hung Hoang <hoangmanhhung008009@gmail.com>
2025-05-23 08:30:31 +0700

    update

--- src/main/java/com/example/BaseCMS/module/product/repo/ProductRepository.java
+++ src/main/java/com/example/BaseCMS/module/product/repo/ProductRepository.java
@@ -21,12 +21,12 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query(
     "SELECT  p FROM Product p " +
     "LEFT JOIN CategoryProduct cp ON cp.productId = p.id " +
-     "LEFT JOIN ProductKeyword pk ON p.id = pk.productId " +
+    "LEFT JOIN ProductKeyword pk ON p.id = pk.productId " +
     "LEFT JOIN Keyword k ON k.id = pk.keywordId " +
     "LEFT JOIN Category c ON c.id = cp.categoryId " +
     "WHERE :categoryId IS NULL OR cp.categoryId = :categoryId " + "AND :brandId IS NULL OR p.brandId = :brandId "+
-    "AND :keyword is null or p.name like concat('%', :keyword, '%') " +
-    "OR :keyword is null or k.name like concat('%', :keyword, '%') " +
+    "AND (:keyword is null or p.name like concat('%', :keyword, '%') " +
+    "OR :keyword is null or k.name like concat('%', :keyword, '%')) " +
     "ORDER BY p.createdAt DESC"
     )
     Page<Product> findProduct(Pageable pageable, Long categoryId, Long brandId, String keyword);
